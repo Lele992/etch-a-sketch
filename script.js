f8b5e8e7f3a970a1dd3699d7c6f05a8cb119dd15
Lele992 <mic92_@msn.com>
2019-06-02 11:42:52 +0200

    Fix JavaScript

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let titleHalf1="ETCH A";
 let titleHalf2="SKETCH";
 createTitle(titleHalf1,title1);
 createTitle(titleHalf2,title2);
+let opacity=0.1;
 
 createGrid(nSquares);
 clearButton.addEventListener("click",clear);
@@ -25,21 +26,25 @@ function paint(e){
     switch(modeSelect.value){
         case "classic":
             e.target.style.background="black";
+            e.target.style.opacity=1;
             break;
         case "brush":
             e.target.style.background="black";
-            e.target.style.opacity=0.1;
-            const style=getComputedStyle(e.target);
-            const op=style.opacity
+            let opacity=Number(e.target.style.opacity);
+            opacity+=0.1;
+            e.target.style.opacity=opacity;
             break;
         case "random":
             e.target.style.background="rgb("+Math.random()*255+", "+Math.random()*255+", "+Math.random()*255+")";
+            e.target.style.opacity=1;
+            break;
     }
 }
 
 function clear(e){
     const colored=Array.from(document.querySelectorAll(".on"));
     colored.forEach(element => {element.style.background=""; element.classList.remove("on");});
+    opacity=0.1;
 }
 
 function newGrid(e){
